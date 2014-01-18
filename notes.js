b31c31daca46944a5b41b51f49d0acacb2d91f43
Omni Adams <omni@digitaldarkness.com>
2014-01-18 13:18:18 -0700

    Initial structure for talk

---
+++ notes.js
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview Speaker notes behavior
+ */
+
+/**
+ * Receive a message from the main window and display it.
+ * @param {Event} event Event that fired this handler (message).
+ */
+var receiveMessage = function(event) {
+    window.console.log(event.data);
+    $('#notes').html(event.data);
+};
+
+window.addEventListener('message', receiveMessage, false);
